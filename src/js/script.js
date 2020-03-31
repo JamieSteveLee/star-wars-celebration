47dc37462ffec4b392d42356729741d78fc8d149
Jamie Lee <jamiestevelee@gmail.com>
2020-04-01 00:18:12 +0100

    Add settings area, and move data to vue data

--- src/js/script.js
+++ src/js/script.js
@@ -3,7 +3,94 @@ var app = new Vue({
 	data: {
 		currentTimezone: 'America/Chicago',
 		selectedChicago: true,
-		selectedLocal: false
+		selectedLocal: false,
+		settingsOpen: false,
+		celebration2019: [
+			{
+				date: "Friday April 12",
+				day: "Day 2",
+				panels: [
+					{
+						name: "Star Wars: Episode IX",
+						timeGMT: [2019, 4, 12, 16, 0, 0],
+						url: "https://www.youtube.com/watch?v=RnhiLZOprZE"
+					},
+					{
+						name: "Vader Immortal: A Star Wars VR Series",
+						timeGMT: [2019, 4, 12, 18, 30, 0],
+						url: "https://www.youtube.com/watch?v=bP9lB8BMSzo"
+					},
+					{
+						name: "The Creatures, Droids & Aliens of Star Wars",
+						timeGMT: [2019, 4, 12, 21, 0, 0],
+						url: "https://www.youtube.com/watch?v=De3NwcdQAlY"
+					},
+				]
+			},
+			{
+				date: "Saturday April 13",
+				day: "Day 3",
+				panels: [
+					{
+						name: "Bringing Star Wars: Galaxy’s Edge to Life at Disney Parks",
+						timeGMT: [2019, 4, 13, 16, 0, 0],
+						url: "https://www.youtube.com/watch?v=lMwhJiK-XbA"
+					},
+					{
+						name: "The Galaxy-Wide Premiere of Star Wars Jedi: Fallen Order",
+						timeGMT: [2019, 4, 13, 18, 30, 0],
+						url: "https://www.youtube.com/watch?v=XNa5FwnCfXE"
+					},
+					{
+						name: "Sisters of the Force, A Celebration of Women and Star Wars",
+						timeGMT: [2019, 4, 13, 21, 0, 0],
+						url: "https://www.youtube.com/watch?v=MfzrCUOx91U"
+					},
+				]
+			},
+			{
+				date: "Sunday April 14",
+				day: "Day 4",
+				panels: [
+					{
+						name: "The Mandalorian",
+						timeGMT: [2019, 4, 14, 16, 0, 0],
+						url: "https://www.youtube.com/watch?v=GrlTosbjylA"
+					},
+					{
+						name: "In Conversation with Alan Tudyk",
+						timeGMT: [2019, 4, 14, 18, 30, 0],
+						url: "https://www.youtube.com/watch?v=rscLMX2axdk"
+					},
+					{
+						name: "Star Wars: The Clone Wars Sneak Peek",
+						timeGMT: [2019, 4, 14, 20, 30, 0],
+						url: "https://www.youtube.com/watch?v=I0XlB2EJZuA"
+					},
+				]
+			},
+			{
+				date: "Monday April 15",
+				day: "Day 5",
+				panels: [
+					{
+						name: "Star Wars: The Phantom Menace 20th Anniversary Celebration",
+						timeGMT: [2019, 4, 15, 16, 0, 0],
+						url: "https://www.youtube.com/watch?v=bhBGb8J9vsI"
+					},
+					{
+						name: "Short Stories with Warwick Davis",
+						timeGMT: [2019, 4, 15, 18, 30, 0],
+						url: ""
+					},
+					{
+						name: "Celebration Chicago Closing Ceremony",
+						timeGMT: [2019, 4, 15, 20, 30, 0],
+						url: "https://www.youtube.com/watch?v=H0WpL1wnAzY"
+					},
+				]
+			},
+		]
 	},
 	computed: {
 		browserTimezone: function() {
@@ -39,6 +126,9 @@ var app = new Vue({
 			}
 
 			this.currentTimezone = newTimezone;
+		},
+		toggleSettings: function() {
+			this.settingsOpen = !this.settingsOpen;
 		}
 	}
 });
