399c9c113da9f1e41fab1401ab7f27b772636382
Jamie Lee <jamiestevelee@gmail.com>
2020-04-08 18:57:20 +0100

    Save settings in localstorage, short names, app icon

--- src/js/script.js
+++ src/js/script.js
@@ -2,11 +2,13 @@ var app = new Vue({
 	el: '#app',
 	data: {
 		settingsOpen: false,
-		settings: {
+		settings: {},
+		defaultSettings: {
 			currentTimezone: 'America/Chicago',
 			selectedChicago: true,
 			selectedLocal: false,
-			twentyFourHour: false
+			twentyFourHour: false,
+			shortNames: false
 		},
 		celebration2019: [
 			{
@@ -15,16 +17,19 @@ var app = new Vue({
 				panels: [
 					{
 						name: "Star Wars: Episode IX",
+						shortName: "Episode IX",
 						timeGMT: [2019, 4, 12, 16, 0, 0],
 						url: "https://www.youtube.com/watch?v=RnhiLZOprZE"
 					},
 					{
 						name: "Vader Immortal: A Star Wars VR Series",
+						shortName: "Vader Immortal",
 						timeGMT: [2019, 4, 12, 18, 30, 0],
 						url: "https://www.youtube.com/watch?v=bP9lB8BMSzo"
 					},
 					{
 						name: "The Creatures, Droids & Aliens of Star Wars",
+						shortName: "Creatures, Droids & Aliens",
 						timeGMT: [2019, 4, 12, 21, 0, 0],
 						url: "https://www.youtube.com/watch?v=De3NwcdQAlY"
 					},
@@ -36,16 +41,19 @@ var app = new Vue({
 				panels: [
 					{
 						name: "Bringing Star Wars: Galaxy’s Edge to Life at Disney Parks",
+						shortName: "Galaxy’s Edge",
 						timeGMT: [2019, 4, 13, 16, 0, 0],
 						url: "https://www.youtube.com/watch?v=lMwhJiK-XbA"
 					},
 					{
 						name: "The Galaxy-Wide Premiere of Star Wars Jedi: Fallen Order",
+						shortName: "Jedi: Fallen Order",
 						timeGMT: [2019, 4, 13, 18, 30, 0],
 						url: "https://www.youtube.com/watch?v=XNa5FwnCfXE"
 					},
 					{
 						name: "Sisters of the Force, A Celebration of Women and Star Wars",
+						shortName: "Women and Star Wars",
 						timeGMT: [2019, 4, 13, 21, 0, 0],
 						url: "https://www.youtube.com/watch?v=MfzrCUOx91U"
 					},
@@ -62,11 +70,13 @@ var app = new Vue({
 					},
 					{
 						name: "In Conversation with Alan Tudyk",
+						shortName: "Alan Tudyk",
 						timeGMT: [2019, 4, 14, 18, 30, 0],
 						url: "https://www.youtube.com/watch?v=rscLMX2axdk"
 					},
 					{
 						name: "Star Wars: The Clone Wars Sneak Peek",
+						shortName: "The Clone Wars",
 						timeGMT: [2019, 4, 14, 20, 30, 0],
 						url: "https://www.youtube.com/watch?v=I0XlB2EJZuA"
 					},
@@ -78,16 +88,19 @@ var app = new Vue({
 				panels: [
 					{
 						name: "Star Wars: The Phantom Menace 20th Anniversary Celebration",
+						shortName: "TPM 20th Anniversary",
 						timeGMT: [2019, 4, 15, 16, 0, 0],
 						url: "https://www.youtube.com/watch?v=bhBGb8J9vsI"
 					},
 					{
 						name: "Short Stories with Warwick Davis",
+						shortName: "Warwick Davis",
 						timeGMT: [2019, 4, 15, 18, 30, 0],
 						url: ""
 					},
 					{
 						name: "Celebration Chicago Closing Ceremony",
+						shortName: "Closing Ceremony",
 						timeGMT: [2019, 4, 15, 20, 30, 0],
 						url: "https://www.youtube.com/watch?v=H0WpL1wnAzY"
 					},
@@ -98,7 +111,6 @@ var app = new Vue({
 	computed: {
 		browserTimezone: function() {
 			return Intl.DateTimeFormat().resolvedOptions().timeZone;
-			console.log();
 		}
 	},
 	methods: {
@@ -159,9 +171,50 @@ var app = new Vue({
 			}
 
 			this.settings.currentTimezone = newTimezone;
+			this.saveSettings();
+		},
+		getDisplayname: function(panel) {
+			if(this.settings.shortNames && panel.shortName) {
+				return panel.shortName;
+			} else {
+				return panel.name;
+			}
 		},
 		toggleSettings: function() {
 			this.settingsOpen = !this.settingsOpen;
+		},
+
+		saveSettings: function() {
+			var saveSettings = JSON.stringify(this.settings);
+			localStorage.setItem('jamie_sw_celebration_2019', saveSettings);
+		},
+		loadSettings: function() {
+			var loadSettings = JSON.parse(localStorage.getItem('jamie_sw_celebration_2019'));
+			if(loadSettings) {
+				this.settings = loadSettings;
+			} else {
+				this.settings = Object.assign({}, this.defaultSettings);
+			}
+		},
+		resetSettings: function() {
+			var confirmReset = confirm('Are you sure you want to clear all settings?');
+			if(confirmReset) {
+				this.settings = Object.assign({}, this.defaultSettings);
+				setTimeout(() => {
+					localStorage.removeItem('jamie_sw_celebration_2019');
+				}, 100);
+			}
 		}
+	},
+	watch: {
+		settings: {
+			handler: function() {
+				this.saveSettings();
+			},
+			deep: true
+		}
+	},
+	beforeMount() {
+		this.loadSettings();
 	}
 });
